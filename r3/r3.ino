5600a8bd42f50a94a720f69d83d671fd05fe6df9
Niriut <80639229+Niriut@users.noreply.github.com>
2021-03-14 23:58:48 +0700

    Add files via upload

---
+++ r3/r3.ino
@@ -0,0 +1,31 @@
+ int sensor1 = A0;
+ int sensor2 = A1;
+ int sensor3 = A2;
+ int sensor4 = A3;
+
+void setup() {
+  pinMode(sensor1, INPUT);
+  pinMode(sensor2, INPUT);
+  pinMode(sensor3, INPUT);
+  pinMode(sensor4, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+   Serial.println("แบตเตอรี่ตัวที่ 1");
+     int val1 = mapf(analogRead(A0), 0, 1024, 0, 24);
+  Serial.println(val1);
+  Serial.println("แบตเตอรี่ตัวที่ 2");
+    int val2 = mapf(analogRead(A1), 0, 1024, 0, 24);
+  Serial.println(val2);
+  Serial.println("แบตเตอรี่ตัวที่ 3");
+    int val3 = mapf(analogRead(A3), 0, 1024, 0, 24);
+  Serial.println(val3);
+  Serial.println("แบตเตอรี่ตัวที่ 4");
+    int val4 = mapf(analogRead(A4), 0, 1024, 0, 24);
+  Serial.println(val4);
+    delay(3000);
+}
+int mapf(int val, int in_min, double in_max, int out_min, int out_max) {
+  return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
